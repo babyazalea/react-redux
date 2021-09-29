6443b03c927a954d1993fc17a4bc86976055292d
babyazalea <usun12@hotmail.com>
2021-09-29 22:26:14 +0900

    added dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,21 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
   // react-redux가 제공하는 useSelector hook은, useSelector hook이 사용된 컴포넌트의 subscription을
   // 자동으로 관리하여, 가장 최신(latest)의 state를 반환해주고, unmount시 자동으로 subscription을 clear한다.
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -13,6 +23,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
