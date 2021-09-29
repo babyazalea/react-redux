b8d1436995ae73568f5814d74d1cb1c3e6bbae64
babyazalea <usun12@hotmail.com>
2021-09-29 22:21:07 +0900

    using redux in component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // react-redux가 제공하는 useSelector hook은, useSelector hook이 사용된 컴포넌트의 subscription을
+  // 자동으로 관리하여, 가장 최신(latest)의 state를 반환해주고, unmount시 자동으로 subscription을 clear한다.
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
